b219cfe998bc62223883898307a19e4595acaa73
aaaeide <ae@pilco.no>
2022-02-07 16:00:53 +0100

    book part 1

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,55 +1,145 @@
-module PhotoGroove exposing (main)
+module PhotoGroove exposing (main, view)
 
+import Array exposing (Array)
 import Browser
-import Html exposing (div, h1, img, text)
+import Html exposing (Html, button, div, h1, img, input, label, text)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onMouseOver)
+import Html.Events exposing (onClick)
+import Random
 
 
+urlPrefix : String
 urlPrefix =
     "http://elm-in-action.com/"
 
 
+viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumb =
     img
         [ src (urlPrefix ++ thumb.url)
         , classList [ ( "selected", selectedUrl == thumb.url ) ]
-        , onMouseOver { action = "mouse_over", data = thumb.url }
+        , onClick (ImgClick thumb.url)
         ]
         []
 
 
+viewSizeChooser : ThumbnailSize -> ThumbnailSize -> Html Msg
+viewSizeChooser selectedSize size =
+    label []
+        [ input
+            [ type_ "radio"
+            , name "size"
+            , onClick (SizeClick size)
+            , checked (selectedSize == size)
+            ]
+            []
+        , text (sizeToString size)
+        ]
+
+
+sizeToString : ThumbnailSize -> String
+sizeToString size =
+    case size of
+        Small ->
+            "small"
+
+        Medium ->
+            "med"
+
+        Large ->
+            "large"
+
+
+view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ]
+        , div [ id "choose-size" ]
+            (List.map (viewSizeChooser model.chosenSize) [ Small, Medium, Large ])
+        , button [ onClick SurpriseClick ] [ text "Surprise me" ]
+        , div [ id "thumbnails", class (sizeToString model.chosenSize) ]
             (List.map (viewThumbnail model.selectedUrl) model.photos)
         , img [ class "large", src (urlPrefix ++ "large/" ++ model.selectedUrl) ] []
         ]
 
 
-initialModel =
-    { photos =
-        [ { url = "1.jpeg" }
-        , { url = "2.jpeg" }
-        , { url = "3.jpeg" }
-        ]
-    , selectedUrl = "1.jpeg"
+type ThumbnailSize
+    = Small
+    | Medium
+    | Large
+
+
+type alias Photo =
+    { url : String }
+
+
+type Status
+    = Loading
+    | Loaded (List Photo) String
+    | Errored String
+
+
+type alias Model =
+    { status : Status
+    , chosenSize : ThumbnailSize
     }
 
 
-update { action, data } model =
-    case action of
-        "mouse_over" ->
-            { model | selectedUrl = data }
-
-        _ ->
-            model
+initialModel : Model
+initialModel =
+    { status = Loading
+    , chosenSize = Medium
+    }
 
 
+photoArray : Array Photo
+photoArray =
+    Array.fromList initialModel.photos
+
+
+getPhotoUrl : Int -> String
+getPhotoUrl index =
+    case Array.get index photoArray of
+        Just photo ->
+            photo.url
+
+        Nothing ->
+            ""
+
+
+randomPhotoPicker : Random.Generator Int
+randomPhotoPicker =
+    Random.int 0 (Array.length photoArray - 1)
+
+
+type Msg
+    = ImgClick String
+    | GotSelectedIndex Int
+    | SurpriseClick
+    | SizeClick ThumbnailSize
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ImgClick url ->
+            ( { model | selectedUrl = url }, Cmd.none )
+
+        GotSelectedIndex index ->
+            ( { model | selectedUrl = getPhotoUrl index }, Cmd.none )
+
+        SurpriseClick ->
+            ( model, Random.generate GotSelectedIndex randomPhotoPicker )
+
+        SizeClick size ->
+            ( { model | chosenSize = size }, Cmd.none )
+
+
+main : Program () Model Msg
 main =
-    Browser.sandbox
-        { init = initialModel
+    Browser.element
+        { init = \flags -> ( initialModel, Cmd.none )
         , view = view
         , update = update
+        , subscriptions = \model -> Sub.none
         }
