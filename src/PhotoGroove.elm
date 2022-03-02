82e13b76ac7add238919f7239b969a527e711512
aaaeide <ae@pilco.no>
2022-03-02 10:47:44 +0100

    Chapter 4.1

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -5,6 +5,7 @@ import Browser
 import Html exposing (Html, button, div, h1, img, input, label, select, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Http
 import Random
 
 
@@ -107,9 +108,10 @@ initialModel =
 
 type Msg
     = ImgClick String
-    | GotRandomPhoto Photo
     | SurpriseClick
     | SizeClick ThumbnailSize
+    | GotRandomPhoto Photo
+    | GotPhotos (Result Http.Error String)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -134,11 +136,26 @@ update msg model =
                 Errored _ ->
                     ( model, Cmd.none )
 
+        SizeClick size ->
+            ( { model | chosenSize = size }, Cmd.none )
+
         GotRandomPhoto photo ->
             ( { model | status = selectUrl photo.url model.status }, Cmd.none )
 
-        SizeClick size ->
-            ( { model | chosenSize = size }, Cmd.none )
+        GotPhotos (Ok responseStr) ->
+            case String.split "," responseStr of
+                (firstUrl :: _) as urls ->
+                    let
+                        photos =
+                            List.map Photo urls
+                    in
+                    ( { model | status = Loaded photos firstUrl }, Cmd.none )
+
+                [] ->
+                    ( { model | status = Errored "No photos found" }, Cmd.none )
+
+        GotPhotos (Err httpError) ->
+            ( { model | status = Errored "Server error!!" }, Cmd.none )
 
 
 selectUrl : String -> Status -> Status
